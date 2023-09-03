076fd0ce297424b7c98e6a5e8e7625691c72ebdd
Itamar Gerasy <itamargerasy@gmail.com>
2023-09-03 22:18:47 +0300

    created SwitchMenu and seperated it from Switch file

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,17 +1,10 @@
 // Switch.tsx
-import React, { ReactNode, useRef } from 'react'
-import DeleteIcon from '@mui/icons-material/Delete';
-import InfoIcon from '@mui/icons-material/Info'
-import { Switch as SwitchType, GlobalStateContextType } from './typeForComponents'
+import React from 'react'
+import { Switch as SwitchType } from './typeForComponents'
 import { Draggable } from "react-beautiful-dnd";
 import { SwitchStyle } from './Switch.styles'
 import { withGlobalState } from './MainAppState';
-import Popper from '@mui/material/Popper';
-import ClickAwayListener from '@mui/material/ClickAwayListener';
-import Grow from '@mui/material/Grow';
-import Paper from '@mui/material/Paper';
-import MenuItem from '@mui/material/MenuItem';
-import MenuList from '@mui/material/MenuList';
+import { SwitchMenu } from './SwitchMenu'
 
 
 type SwitchProps = {
@@ -22,17 +15,20 @@ type SwitchProps = {
 export const Switch = (props: SwitchProps) => {
     const [menuOpen, setMenuOpen] = React.useState(false);
     const menuAnchorRef = React.useRef<HTMLDivElement>(null); // refrence for the anchor of the menu
-    
+    const menuElemetRef = React.useRef<HTMLDivElement>(null); // refrence for the anchor of the menu
+
     const handleToggleMenu = () => {
         setMenuOpen((prevOpen) => !prevOpen);
     };
     
     const handleMenuClose = (event: Event | React.SyntheticEvent) => {
         if (
-        menuAnchorRef.current &&
-        menuAnchorRef.current.contains(event.target as HTMLElement)
+            menuAnchorRef.current &&
+            menuAnchorRef.current.contains(event.target as HTMLElement) &&
+            menuElemetRef.current?.contains(event.target as HTMLElement)
         ) {
-        return;
+            console.log("not gonna close")
+            return;
         }
     
         setMenuOpen(false);
@@ -69,43 +65,7 @@ export const Switch = (props: SwitchProps) => {
                 switchSize={props.switch.size}
                 >  
                 </SwitchStyle>
-                <Popper
-                    open={menuOpen}
-                    anchorEl={menuAnchorRef.current}
-                    role={undefined}
-                    placement="left"
-                    transition
-                    disablePortal
-                    style={{
-                        zIndex: 9999, //  high z-index value
-                        width: 20,
-                        height: 20,
-                    }}
-                    >
-                    {({ TransitionProps, placement }) => (
-                        <Grow
-                        {...TransitionProps}
-                        style={{
-                            transformOrigin:
-                            placement === 'bottom-start' ? 'left top' : 'left bottom',
-                        }}
-                        >
-                        <Paper>
-                            <ClickAwayListener onClickAway={handleMenuClose}>
-                            <MenuList
-                                autoFocusItem={menuOpen}
-                                id="composition-menu"
-                                aria-labelledby="composition-button"
-                                onKeyDown={handleListKeyDown}
-                            >
-                                <MenuItem sx={{padding: 0, paddingBottom: 1, display: "flex", justifyContent:'center', flexDirection: 'row'}}><InfoIcon sx={{fontSize: 10, padding: 0}}/></MenuItem>
-                                <MenuItem sx={{padding: 0, display: "flex", justifyContent:'center', flexDirection: 'row'}} onClick={handleMenuClose} ><DeleteIcon sx={{fontSize: 10, padding: 0}}/></MenuItem>
-                            </MenuList>
-                            </ClickAwayListener>
-                        </Paper>
-                        </Grow>
-                    )}
-                </Popper>
+                <SwitchMenu menuOpen={menuOpen} menuAnchorRef={menuAnchorRef} handleMenuClose={handleMenuClose} handleListKeyDown={handleListKeyDown}/>
             </div>
         )}
         </Draggable>
