428c1081035a186bed2bcb33707197ca5ebd6b20
Itamar Gerasy <itamargerasy@gmail.com>
2023-09-09 10:37:11 +0300

    changed some divs into styled components

--- src/components/general/GeneralStyles.styles.tsx
+++ src/components/general/GeneralStyles.styles.tsx
@@ -5,3 +5,14 @@ export const Title = styled.h6(() => ({
     padding: '8px',
     textAlign: 'center'
 }));
+
+export const FlexBox = styled.div({
+    display: 'flex',
+    flex: 1,
+})
+
+export const ColumnFlexBox = styled.div({
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+})
--- src/components/general/InfoDragRemove.tsx
+++ src/components/general/InfoDragRemove.tsx
@@ -5,6 +5,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import DragHandleIcon from '@mui/icons-material/DragHandle';
 import { Info } from './Info'
 import InfoIcon from '@mui/icons-material/Info'
+import { FlexBox, ColumnFlexBox } from '../general/GeneralStyles.styles'
 
 type InfoDragRemoveProps = {
     provided: DraggableProvided;
@@ -12,19 +13,14 @@ type InfoDragRemoveProps = {
   };
   
 export const InfoDragRemove: React.FC<InfoDragRemoveProps> = ({ provided, infoStr }) => {
-    // const [isInfoOpen, setIsInfoOpen] = useState(false);
-
-    // const handleInfoClick = () => {
-    //     setIsInfoOpen(!isInfoOpen);
-    // };
 
     return (
-        <div style={{backgroundColor: 'lightblue', display: 'flex', flexDirection: 'column', height: '100%'}}>
-            <div {...provided.dragHandleProps} style={{display: 'flex'}}>
+        <ColumnFlexBox style={{backgroundColor: 'lightblue'}}>
+            <FlexBox {...provided.dragHandleProps}>
                 <DragHandleIcon sx={{ fontSize: '15px' }} />
-            </div>
+            </FlexBox>
             <InfoIcon sx={{ fontSize: '15px' }}/>
             <DeleteIcon sx={{ fontSize: '15px' }} />
-        </div>
+        </ColumnFlexBox>
     );
 };
