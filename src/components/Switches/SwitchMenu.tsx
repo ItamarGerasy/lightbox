f579efd99073e766911f36f6d34e18b187ee137a
Itamar Gerasy <itamargerasy@gmail.com>
2024-08-13 15:57:25 +0200

    changed switchMenu icons to outlines icons

--- src/components/Switches/SwitchMenu.tsx
+++ src/components/Switches/SwitchMenu.tsx
@@ -1,32 +1,32 @@
 // SwitchMenu.tsx
 import React from 'react'
-import DeleteIcon from '@mui/icons-material/Delete';
-import InfoIcon from '@mui/icons-material/Info'
-import Popper from '@mui/material/Popper';
-import ClickAwayListener from '@mui/material/ClickAwayListener';
-import Grow from '@mui/material/Grow';
-import Paper from '@mui/material/Paper';
-import MenuItem from '@mui/material/MenuItem';
-import MenuList from '@mui/material/MenuList';
-import { RefObject, LegacyRef } from 'react';
+import Popper from '@mui/material/Popper'
+import ClickAwayListener from '@mui/material/ClickAwayListener'
+import Grow from '@mui/material/Grow'
+import Paper from '@mui/material/Paper'
+import MenuItem from '@mui/material/MenuItem'
+import MenuList from '@mui/material/MenuList'
+import { RefObject, LegacyRef } from 'react'
 import { Switch as SwitchType } from '../../framework/Switch'
-import { useBoard } from '../../hooks/BoardHook';
-import { FlexBox, InfoWindowStyle, smallIcon } from '../general/GeneralStyles.styles';
+import { useBoard } from '../../hooks/BoardHook'
+import { FlexBox, InfoWindowStyle, smallIcon } from '../general/GeneralStyles.styles'
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined'
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
 
 type SwitchMenuProps = {
-    menuOpen: boolean;
-    menuAnchorRef: RefObject<HTMLDivElement>;
-    handleMenuClose: (event: Event | React.SyntheticEvent) => void;
+    menuOpen: boolean
+    menuAnchorRef: RefObject<HTMLDivElement>
+    handleMenuClose: (event: Event | React.SyntheticEvent) => void
     // @ts-ignore: Ignore the "Type 'KeyboardEvent' is not generic" TypeScript error
-    handleListKeyDown: (event: KeyboardEvent<Element>) => void;
-    switch: SwitchType;
+    handleListKeyDown: (event: KeyboardEvent<Element>) => void
+    switch: SwitchType
 }
 
 export const SwitchMenu = (props: SwitchMenuProps) => {
     const {actions} = useBoard()
-    const [infoAnchorEl, setInfoAnchorEl] = React.useState<null | HTMLElement>(null);
-    const open = Boolean(infoAnchorEl);
-    const id = open ? 'switch-info-popper' : undefined;
+    const [infoAnchorEl, setInfoAnchorEl] = React.useState<null | HTMLElement>(null)
+    const open = Boolean(infoAnchorEl)
+    const id = open ? 'switch-info-popper' : undefined
 
     const handleInfoClose = (event: Event | React.SyntheticEvent) => {
         setInfoAnchorEl(null)
@@ -36,8 +36,8 @@ export const SwitchMenu = (props: SwitchMenuProps) => {
 
     const handleClick = (event: React.MouseEvent<HTMLElement>) => {
         props.handleMenuClose(event)
-        setInfoAnchorEl(infoAnchorEl ? null : event.currentTarget);
-    };
+        setInfoAnchorEl(infoAnchorEl ? null : event.currentTarget)
+    }
 
     const generateSwitchInfoString = () => {
         const s = props.switch
@@ -87,13 +87,13 @@ export const SwitchMenu = (props: SwitchMenuProps) => {
                                 sx={{padding: 0, paddingTop: "4px", paddingBottom: 1, display: "flex", justifyContent:'center', flexDirection: 'row'}} 
                                 onClick={handleClick}
                             >
-                                <InfoIcon sx={smallIcon} />
+                                <InfoOutlinedIcon sx={smallIcon} />
                             </MenuItem>
                             <MenuItem 
                                 sx={{padding: 0, paddingBottom: "4px", display: "flex", justifyContent:'center', flexDirection: 'row'}} 
                                 onClick={handleDelete}
                             >
-                                <DeleteIcon sx={smallIcon}/>
+                                <DeleteOutlineOutlinedIcon sx={smallIcon}/>
                             </MenuItem>
                         </MenuList>
                         </ClickAwayListener>
